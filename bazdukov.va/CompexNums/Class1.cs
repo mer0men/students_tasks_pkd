438b51badc06d5af0413cae639124d91d440934b
bazdu <bazdukovvalentin@gmail.com>
2022-06-06 12:25:23 +1000

    change field type. change ToComplexString. add Pow Method

--- bazdukov.va/CompexNums/Class1.cs
+++ bazdukov.va/CompexNums/Class1.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Net.Sockets;
-
 namespace CompexNums
 {
     public class Complex
     {
-        private float real;
-        private float img;
+        private double real;
+        private double img;
 
-        public Complex(float _real, float _img)
+        public Complex(double real, double img)
         {
-            real = _real;
-            img = _img;
+            real = real;
+            img = img;
         }
         public static Complex operator +(Complex first, Complex second)
         {
@@ -23,19 +21,26 @@ namespace CompexNums
         }
         public static Complex operator *(Complex first, Complex second)
         {
-            float real = first.real * second.real - first.img * second.img;
-            float img = first.real * second.img + first.img * second.real;
+            double real = first.real * second.real - first.img * second.img;
+            double img = first.real * second.img + first.img * second.real;
             return new Complex(real, img);
         }
         public static Complex operator /(Complex first, Complex second)
         {
-            float real = (first.real * second.real + first.img * second.img) / (second.real * second.real + second.img * second.img);
-            float img = (second.real * first.img - first.real * second.img) / (second.real * second.real + second.img * second.img);
+            double real = (first.real * second.real + first.img * second.img) / (second.real * second.real + second.img * second.img);
+            double img = (second.real * first.img - first.real * second.img) / (second.real * second.real + second.img * second.img);
             return new Complex(real, img);
         }
-        public string ToString(Complex num)
+        public string ToComplexString()
         {
-            return $"{num.real} + {num.img}i";
+            return this.real > 0 ? $"{this.real}+{this.img}i" : $"{this.real}{this.img}i";
+        }
+        public Complex ComplexPow(double n)
+        {
+           double module = Math.Sqrt(this.real * this.real + this.img * this.img);
+           double argue = Math.Atan(this.img / this.real);
+           Complex z = new Complex(module * Math.Cos(argue), module * Math.Sin(argue));
+           return new Complex(z.real * n, z.img * n);
         }
         
     }
