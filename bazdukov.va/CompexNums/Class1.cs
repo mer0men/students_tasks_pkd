9284b3b1cf0596f506358f410ecc6bd06f929d10
bazdu <bazdukovvalentin@gmail.com>
2022-06-06 10:48:16 +1000

    Add fields, constructor and simple operations methods

--- bazdukov.va/CompexNums/Class1.cs
+++ bazdukov.va/CompexNums/Class1.cs
@@ -1,7 +1,38 @@
-﻿namespace CompexNums
+﻿using System;
+using System.Net.Sockets;
+
+namespace CompexNums
 {
     public class Complex
     {
+        private float real;
+        private float img;
+
+        public Complex(float _real, float _img)
+        {
+            real = _real;
+            img = _img;
+        }
+        public static Complex operator +(Complex first, Complex second)
+        {
+            return new Complex(first.real + second.real, first.img + second.img);
+        }
+        public static Complex operator -(Complex first, Complex second)
+        {
+            return new Complex(first.real - second.real, first.img - second.img);
+        }
+        public static Complex operator *(Complex first, Complex second)
+        {
+            float real = first.real * second.real - first.img * second.img;
+            float img = first.real * second.img + first.img * second.real;
+            return new Complex(real, img);
+        }
+        public static Complex operator /(Complex first, Complex second)
+        {
+            float real = (first.real * second.real + first.img * second.img) / (second.real * second.real + second.img * second.img);
+            float img = (second.real * first.img - first.real * second.img) / (second.real * second.real + second.img * second.img);
+            return new Complex(real, img);
+        }
         
     }
 }
